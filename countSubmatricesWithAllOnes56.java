161e936b702c6e97325a890dfded59ebf5d6c90a
pratik-kadlak <141506069+pratik-kadlak@users.noreply.github.com>
2024-08-10 00:28:45 +0530

    video_56

---
+++ countSubmatricesWithAllOnes56.java
@@ -0,0 +1,31 @@
+public class countSubmatricesWithAllOnes56 {
+    
+    public static int countSquares(int[][] matrix) {
+        int m = matrix.length; 
+        int n = matrix[0].length;
+        int[][] dp = new int[m+1][n+1];
+
+        int ans = 0;
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(matrix[i-1][j-1] == 1){
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]))+1;
+                    ans += dp[i][j];
+                }
+            }
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                            {0,1,1,1},
+                            {1,1,1,1},
+                            {0,1,1,1}
+                        };
+
+        System.out.println(countSquares(matrix));
+    }
+
+}
